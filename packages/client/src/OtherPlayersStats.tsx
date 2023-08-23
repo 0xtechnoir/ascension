a51288d8b8e795c20e54556822da46f8cc8524d5
0xNaylor <0xnaylor@gmail.com>
2023-08-23 21:29:59 +0100

    Updated the rendering of OtherPlayerStats to try and fix console wanring about different number of hooks being called on subsequent rendering cycles  as new players join

--- packages/client/src/OtherPlayersStats.tsx
+++ packages/client/src/OtherPlayersStats.tsx
@@ -18,49 +18,46 @@ export const OtherPlayersStats: React.FC<OtherPlayersStatsProps> = ({ handleErro
         systemCalls: { sendActionPoint, attack },
     } = useMUD();
 
-    const mappedPlayers = players.map((player) => {
-        const entity = player;
-        const username = useComponentValue(Username, player)?.value;
-        const health = useComponentValue(Health, player)?.value;
-        const range = useComponentValue(Range, player)?.value;
-        const ap = useComponentValue(ActionPoint, player)?.value;
-        const alive = useComponentValue(Alive, player)?.value;
-
-        return {
-          entity,
-          username: username,
-          health: health,
-          range: range,
-          actionPoints: ap,
-          alive: alive,
-        };
-    });
+    const MAX_PLAYERS = 10;
+    const placeholderPlayer = {};
+    const preAllocatedPlayers = new Array(MAX_PLAYERS).fill(placeholderPlayer);
+    const mergedPlayers = [...players, ...preAllocatedPlayers].slice(0, MAX_PLAYERS);
 
     return (
       <div className="flex flex-col items-start" style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px', margin: '16px 0' }}>
         <h2>Other Players:</h2>
         <br />
-        {mappedPlayers.map((mappedPlayer) => {
+        {mergedPlayers.map((player) => {
+          if (player === placeholderPlayer) {
+            return null;  // Don't render anything for placeholders
+          } 
+          const entity = player;
+          const username = useComponentValue(Username, player)?.value;
+          const health = useComponentValue(Health, player)?.value;
+          const range = useComponentValue(Range, player)?.value;
+          const ap = useComponentValue(ActionPoint, player)?.value;
+          const alive = useComponentValue(Alive, player)?.value;
+
           return (
             <div
-              key={mappedPlayer.entity}
+              key={entity}
               style={{
-                backgroundColor: mappedPlayer.entity === highlightedPlayer ? "darkgray" : "transparent",
+                backgroundColor: entity === highlightedPlayer ? "darkgray" : "transparent",
                 cursor: 'pointer'
               }}
               onClick={() => {
-                if (highlightedPlayer === mappedPlayer.entity) {
+                if (highlightedPlayer === entity) {
                   setHighlightedPlayer(null);
                 } else {
-                  setHighlightedPlayer(mappedPlayer.entity);
+                  setHighlightedPlayer(entity);
                 }
               }}
             >
-              <p>Name: {mappedPlayer.username}</p>
-              <p>Status: {mappedPlayer.alive ? `Alive` : `Dead`}</p>
-              <p>Health: {mappedPlayer.health}</p>
-              <p>Range: {mappedPlayer.range}</p>
-              <p>Action Points: {mappedPlayer.actionPoints}</p>
+              <p>Name: {username}</p>
+              <p>Status: {alive ? `Alive` : `Dead`}</p>
+              <p>Health: {health}</p>
+              <p>Range: {range}</p>
+              <p>Action Points: {ap}</p>
               <div>
                 <button 
                   className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-300"
